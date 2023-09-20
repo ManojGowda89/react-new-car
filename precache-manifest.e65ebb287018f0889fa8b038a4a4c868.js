4ed678a0aba3a3cb824f43e90d4b7c75d14ae7d5
ManojGowda89 <manojgowdabr89@gmail.com>
2023-09-20 19:18:47 +0530

    Updates

---
+++ precache-manifest.e65ebb287018f0889fa8b038a4a4c868.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e59695c7bded238658cd702ec2ee59",
+    "url": "/react-car-dealer/index.html"
+  },
+  {
+    "revision": "32abe2bf12d3aea41fc6",
+    "url": "/react-car-dealer/static/css/main.447c5b03.chunk.css"
+  },
+  {
+    "revision": "7fb7d257ef9c739d7ff4",
+    "url": "/react-car-dealer/static/js/2.50184ea9.chunk.js"
+  },
+  {
+    "revision": "32abe2bf12d3aea41fc6",
+    "url": "/react-car-dealer/static/js/main.d0a796ba.chunk.js"
+  },
+  {
+    "revision": "ae3fe4fe1c040f6d07c4",
+    "url": "/react-car-dealer/static/js/runtime-main.6b00315b.js"
+  },
+  {
+    "revision": "f3584263b34f90460eb2716aff85ca5d",
+    "url": "/react-car-dealer/static/media/bcgHeroPic.f3584263.jpg"
+  },
+  {
+    "revision": "b962c2c38624d7e607dbde7761165169",
+    "url": "/react-car-dealer/static/media/car-1.b962c2c3.jpeg"
+  },
+  {
+    "revision": "a2917aa0437f41f992418b49cba2dff2",
+    "url": "/react-car-dealer/static/media/car-10.a2917aa0.jpg"
+  },
+  {
+    "revision": "a4c25c7e23c1228e2e6b8f3d5f1a898b",
+    "url": "/react-car-dealer/static/media/car-11.a4c25c7e.jpg"
+  },
+  {
+    "revision": "0cdff85fad91515788287f99e0979e2a",
+    "url": "/react-car-dealer/static/media/car-12.0cdff85f.jpg"
+  },
+  {
+    "revision": "c2ba598d3640119205ed36f79a9d3142",
+    "url": "/react-car-dealer/static/media/car-14.c2ba598d.jpg"
+  },
+  {
+    "revision": "7aac585b7f7641ee576322feacdffbe1",
+    "url": "/react-car-dealer/static/media/car-2.7aac585b.jpg"
+  },
+  {
+    "revision": "418cb1072a86c181279619e9df045a9a",
+    "url": "/react-car-dealer/static/media/car-22.418cb107.jpg"
+  },
+  {
+    "revision": "4214d329d9f38a11ff336dcba0688f0a",
+    "url": "/react-car-dealer/static/media/car-3.4214d329.jpg"
+  },
+  {
+    "revision": "2145bde8e401ce30378180a695ca5633",
+    "url": "/react-car-dealer/static/media/car-4.2145bde8.jpg"
+  },
+  {
+    "revision": "9c0f51e819188a1b139384e63d8cca8f",
+    "url": "/react-car-dealer/static/media/car-5.9c0f51e8.jpg"
+  },
+  {
+    "revision": "b3aae8f15c4b1e1ee0668c62d55d5cc3",
+    "url": "/react-car-dealer/static/media/car-6.b3aae8f1.jpg"
+  },
+  {
+    "revision": "06cd0487a9443120526ac4acf880bf12",
+    "url": "/react-car-dealer/static/media/car-7.06cd0487.jpg"
+  },
+  {
+    "revision": "63c3104f341be9cd0d7d5ce53414ea9c",
+    "url": "/react-car-dealer/static/media/car-8.63c3104f.jpg"
+  },
+  {
+    "revision": "eec5aae2220c69fa76c1c416e4ed4ee4",
+    "url": "/react-car-dealer/static/media/car-9.eec5aae2.jpg"
+  },
+  {
+    "revision": "730fdb1059ab0ee66ed9668d48fc686b",
+    "url": "/react-car-dealer/static/media/details-10.730fdb10.jpg"
+  },
+  {
+    "revision": "26c5b3d7d407874490d8015119ebda2f",
+    "url": "/react-car-dealer/static/media/details-11.26c5b3d7.jpg"
+  },
+  {
+    "revision": "f5589ba3efd163bfd1b5bf766729aa9d",
+    "url": "/react-car-dealer/static/media/details-12.f5589ba3.jpg"
+  },
+  {
+    "revision": "af026b5f95b2e34d6ad555c0c1cead2c",
+    "url": "/react-car-dealer/static/media/details-13.af026b5f.jpg"
+  },
+  {
+    "revision": "96aa63031eb97690a2987b97043c9be0",
+    "url": "/react-car-dealer/static/media/details-14.96aa6303.jpg"
+  },
+  {
+    "revision": "bd803039edd3f47d3650508452184811",
+    "url": "/react-car-dealer/static/media/details-15.bd803039.jpg"
+  },
+  {
+    "revision": "db35ac69befe886a7e238fd09c4ed4dc",
+    "url": "/react-car-dealer/static/media/details-16.db35ac69.jpg"
+  },
+  {
+    "revision": "976140f321682779572e90fe3e1954e1",
+    "url": "/react-car-dealer/static/media/details-17.976140f3.jpg"
+  },
+  {
+    "revision": "0059a266bb375b0d3036d26c149aa798",
+    "url": "/react-car-dealer/static/media/details-18.0059a266.jpg"
+  },
+  {
+    "revision": "4111ffdcdcca6cef72f7e4cdcec94663",
+    "url": "/react-car-dealer/static/media/details-19.4111ffdc.jpg"
+  },
+  {
+    "revision": "d2702ed95a3f8f36e835dc3a273f5dba",
+    "url": "/react-car-dealer/static/media/details-2.d2702ed9.jpeg"
+  },
+  {
+    "revision": "22dab1cd0cd7519f8109fc0201f744ec",
+    "url": "/react-car-dealer/static/media/details-20.22dab1cd.jpg"
+  },
+  {
+    "revision": "a581874b0a5fe7aef00dc1bf8159923b",
+    "url": "/react-car-dealer/static/media/details-21.a581874b.jpg"
+  },
+  {
+    "revision": "8727b4d59db135ba5ad4756291548c14",
+    "url": "/react-car-dealer/static/media/details-22.8727b4d5.jpg"
+  },
+  {
+    "revision": "b24549b29a8e4af79c055d896699b817",
+    "url": "/react-car-dealer/static/media/details-23.b24549b2.jpg"
+  },
+  {
+    "revision": "21615e72dc0a994b593112ea71d3de4a",
+    "url": "/react-car-dealer/static/media/details-24.21615e72.jpg"
+  },
+  {
+    "revision": "dcbc756b3dff8ea6b3fedccc9bf17dc6",
+    "url": "/react-car-dealer/static/media/details-25.dcbc756b.jpg"
+  },
+  {
+    "revision": "64563789a3cc5d6d88fc72dc224e595e",
+    "url": "/react-car-dealer/static/media/details-26.64563789.jpg"
+  },
+  {
+    "revision": "2153880bb1032cbb394e03bcec74673a",
+    "url": "/react-car-dealer/static/media/details-27.2153880b.jpg"
+  },
+  {
+    "revision": "78aab50cdea66ee5f57bfd7dfa6ddb8b",
+    "url": "/react-car-dealer/static/media/details-28.78aab50c.jpg"
+  },
+  {
+    "revision": "54e64dff98fd8f614ad2385332b63c86",
+    "url": "/react-car-dealer/static/media/details-29.54e64dff.jpg"
+  },
+  {
+    "revision": "bb02535cd4c60a3342d8a1342770f637",
+    "url": "/react-car-dealer/static/media/details-3.bb02535c.jpg"
+  },
+  {
+    "revision": "a87a602f6e5f40b08a8e1746e7516ff2",
+    "url": "/react-car-dealer/static/media/details-30.a87a602f.jpg"
+  },
+  {
+    "revision": "e3f0f8f7c0840c83a2a33f4aed7191aa",
+    "url": "/react-car-dealer/static/media/details-31.e3f0f8f7.jpg"
+  },
+  {
+    "revision": "75f1b75462b8c8c4bf3b2e0d69f2c766",
+    "url": "/react-car-dealer/static/media/details-32.75f1b754.jpg"
+  },
+  {
+    "revision": "a6e94d403f72f41411a3597ed1d5a5d4",
+    "url": "/react-car-dealer/static/media/details-33.a6e94d40.jpg"
+  },
+  {
+    "revision": "c2533a3e908cfbd51b4b05c70af73671",
+    "url": "/react-car-dealer/static/media/details-34.c2533a3e.jpg"
+  },
+  {
+    "revision": "458ac9a7647ebca205918d381efb90d7",
+    "url": "/react-car-dealer/static/media/details-35.458ac9a7.jpg"
+  },
+  {
+    "revision": "82297b78d8d13616be77af4f7be5a39a",
+    "url": "/react-car-dealer/static/media/details-36.82297b78.jpg"
+  },
+  {
+    "revision": "49360a407b4d10c8fe55900551099690",
+    "url": "/react-car-dealer/static/media/details-37.49360a40.jpg"
+  },
+  {
+    "revision": "6c573a3ef3589633df4ef2a4ea5843e0",
+    "url": "/react-car-dealer/static/media/details-38.6c573a3e.jpg"
+  },
+  {
+    "revision": "88101c46ed980125bc1c62cd5ea26a00",
+    "url": "/react-car-dealer/static/media/details-39.88101c46.jpg"
+  },
+  {
+    "revision": "030da69f0c905275ae030c5d7ed91374",
+    "url": "/react-car-dealer/static/media/details-4.030da69f.jpg"
+  },
+  {
+    "revision": "354edabfeea42ad193d8149bbf1252ce",
+    "url": "/react-car-dealer/static/media/details-40.354edabf.jpg"
+  },
+  {
+    "revision": "c9d8d8e0ac4e49ca474080713b408781",
+    "url": "/react-car-dealer/static/media/details-5.c9d8d8e0.jpg"
+  },
+  {
+    "revision": "9ca92aee1ac2872e4b63bae5e5b131fa",
+    "url": "/react-car-dealer/static/media/details-6.9ca92aee.jpg"
+  },
+  {
+    "revision": "3904c01f5b228a84478e73f7464c16f1",
+    "url": "/react-car-dealer/static/media/details-7.3904c01f.jpg"
+  },
+  {
+    "revision": "370e3a988e71e9b914f9b48c1713382e",
+    "url": "/react-car-dealer/static/media/details-8.370e3a98.jpg"
+  },
+  {
+    "revision": "60b5ecd0f42e67b3b78aed9e22e60ec5",
+    "url": "/react-car-dealer/static/media/details-9.60b5ecd0.jpg"
+  },
+  {
+    "revision": "47b49276e2dcd396b02fcefc03f46466",
+    "url": "/react-car-dealer/static/media/loading-gear.47b49276.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/react-car-dealer/static/media/logo.9f0cccc1.svg"
+  }
+]);
